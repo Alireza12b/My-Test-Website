9a5f2f1a6f7b9a47d370dcb1d901cc47404ca9e5
Alireza Bagheri <alirezabagheri12b@gmail.com>
2023-07-23 00:59:10 +0330

    razor page projecet adde and login and register finished

---
+++ MyWebsiteRazor/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyWebsiteRazor.Areas.Identity.Data;
+
+namespace MyWebsiteRazor.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SignInManager<ApplicationUser> _signinManager;
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger, SignInManager<ApplicationUser> signinManager)
+        {
+            _signinManager = signinManager;
+            _logger = logger;
+        }
+
+        public IActionResult OnGet()
+        {
+            if (!_signinManager.IsSignedIn(User))
+            {
+                return LocalRedirect("/Identity/Account/Login");
+            }
+
+            return Page();
+        }
+    }
+}
